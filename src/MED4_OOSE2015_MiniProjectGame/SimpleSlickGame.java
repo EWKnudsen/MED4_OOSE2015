db69718230d72a91a4603d23ddd2966a58dec71a
Bredo <ChristofferBredo@hotmail.com>
2015-03-30 14:44:38 +0200

    This time I REALLY imported java.lang.Object

--- src/MED4_OOSE2015_MiniProjectGame/SimpleSlickGame.java
+++ src/MED4_OOSE2015_MiniProjectGame/SimpleSlickGame.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import java.lang.Object.*;
 
 public class SimpleSlickGame extends BasicGame
 {
