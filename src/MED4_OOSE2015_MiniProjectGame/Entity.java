3f03a56596fd69386f9bce30b6d1d4de56d9759c
EWKnudsen <ewknudsen@gmail.com>
2015-04-03 19:46:04 +0200

    fixed the collision

--- src/MED4_OOSE2015_MiniProjectGame/Entity.java
+++ src/MED4_OOSE2015_MiniProjectGame/Entity.java
@@ -1,12 +1,13 @@
 package MED4_OOSE2015_MiniProjectGame;
 
 import java.awt.List;
+import java.util.ArrayList;
 
 public class Entity 
 {
 	private int positionX, positionY;
 	private int speedX, speedY;
-	private int scale;
+	private int hitboxRadius;
 	
 	Entity()
 	{
@@ -16,22 +17,55 @@ public class Entity
 	
 	public boolean collides (Entity other)
 	{
-	//	if(SOMETHING) {SOMETHING}
-		return false;
+		double distance = Math.sqrt(Math.pow(this.getPositionX() - other.getPositionX(), 2) + Math.pow(this.getPositionY() - other.getPositionY(), 2));
+		int collisionDistance = this.getHitboxRadius() + other.getHitboxRadius();
+
+		return distance < collisionDistance;
 	}
 	
-	/*
-	public boolean collides(List<Entity> others)
+	public boolean collides(ArrayList<Entity> others)
 	{
 		for(Entity o:others)
 		{
-			if(this.collides(o) && this != o)
+			if(this != o && this.collides(o))
 			{
 				return true;
-			}
-			return false;
+			}			
 		}
+		return false;
 	}
-	*/
+
+
+	
+	public int getHitboxRadius() {   return hitboxRadius;   }
+
+
+	public void setHitboxRadius(int hitboxRadius) {
+		this.hitboxRadius = hitboxRadius;
+	}
+
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+	
+	
+	
+	
 	
 }
