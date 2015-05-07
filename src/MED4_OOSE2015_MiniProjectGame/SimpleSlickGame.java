faead03e008ae919bdf9d0d470b75b9bb4b15f8e
Bredo <ChristofferBredo@hotmail.com>
2015-05-07 15:43:49 +0200

    Removed unused imports

--- src/MED4_OOSE2015_MiniProjectGame/SimpleSlickGame.java
+++ src/MED4_OOSE2015_MiniProjectGame/SimpleSlickGame.java
@@ -11,9 +11,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class SimpleSlickGame extends BasicGame
