f1d203ed7d04096932bbe47d5a3b92f0848861d5
Bredo <ChristofferBredo@hotmail.com>
2015-05-07 12:16:40 +0200

    Added comments through the Entity class (almost done).

--- src/MED4_OOSE2015_MiniProjectGame/Entity.java
+++ src/MED4_OOSE2015_MiniProjectGame/Entity.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.particles.ParticleSystem;
 
 public class Entity 
 {
+	//Variables for all entities and children classes of entities
 	protected final SimpleSlickGame game;
 	private int positionX, positionY;
 	private int hitboxRadius;
@@ -14,8 +15,10 @@ public class Entity
 	protected ParticleSystem particles;
 	protected ConfigurableEmitter emitter;
 	
+	//Boolean to check if entity is alive
 	protected boolean isAlive = true;
 	
+	//Constructor that sets the X and Y position of the entity and refers to the slick game in to reach its variables and functions.
 	Entity(SimpleSlickGame game, int xPos, int yPos)
 	{
 		this.game = game;
@@ -23,32 +26,39 @@ public class Entity
 		positionY = yPos;
 	}
 	
+	//particleUpdate() is used in children classes to update particles.
 	public void particleUpdate()
 	{
 	}
 	
+	//close() is used to close the entities so the key and mouse listeners is no longer working with them. (Still not functional)
 	public void close()
 	{
 	}
 	
+	//move() is used by children classes to control the movement of entities
 	public void move()
 	{
 	}
 	
+	//spriteSwitch() is used by children classes to switch between their sprites when needed
 	public void spriteSwitch()
 	{	
 	}
 	
+	//Collision() is used to generate an enitity specific action if this entity collides with another specific entity
 	public void Collision(Entity e)
 	{
 		
 	}
 	
+	//renderParticles is used by children classes to render their particle effects
 	public void renderParticles()
 	{
 		
 	}
 	
+	//collides(Entity other) is used to check whether two entities collide or not. It returns a boolean to determine this.
 	public boolean collides (Entity other)
 	{
 		double distance = Math.sqrt(Math.pow(this.getPositionX() - other.getPositionX(), 2) + Math.pow(this.getPositionY() - other.getPositionY(), 2));
@@ -57,6 +67,7 @@ public class Entity
 		return distance <= collisionDistance;
 	}
 	
+	//collides(ArrayList<Entity> others) goes through all entities and determines if they collide. Returns a boolean.
 	//returned a bool before
 	public boolean collides (ArrayList<Entity> others)
 	{
